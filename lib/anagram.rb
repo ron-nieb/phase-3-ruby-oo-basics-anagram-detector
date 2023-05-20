20c95f47dfd7af665c176187cc1c23c20371bf06
Ronald Barasa <barasaogoro@gmail.com>
2023-05-21 00:26:56 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word.downcase
+  end
+
+  def match(words)
+    words.select { |w| anagram?(w.downcase) }
+  end
+
+  private
+
+  def anagram?(other_word)
+    return false if @word == other_word
+
+    sort_chars(@word) == sort_chars(other_word)
+  end
+
+  def sort_chars(word)
+    word.chars.sort
+  end
+end
